20909a66baaba8515dfa89a163a259b9c33bca7f
framarcol <05poner-agentes@icloud.com>
2023-03-20 00:35:40 +0900

    j

--- dockerNGIX/init-letsencrypt.sh
+++ dockerNGIX/init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 domains=(nakama.orbofi.com www.nakama.orbofi.com)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
+email="fran@orbofi.com"
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
